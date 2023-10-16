fb735b1869ddbe37ac09c30d06d3df435ff31b3a
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-16 19:40:14 -0300

    add format checker to the published date

--- classes/item.rb
+++ classes/item.rb
@@ -1,9 +1,13 @@
 require 'securerandom'
 
 class Item
-  attr_reader :genre, :author, :source, :lablel, :publish_date, :archived
+  attr_reader :genre, :author, :source, :lablel, :publish_date
 
   def initialize(publish_date, archived: false)
+    unless publish_date.match?(/^\d{4}-\d{2}-\d{2}$/)
+      raise ArgumentError, 'Invalid publish_date format. Please use YYYY-MM-DD.'
+    end
+
     @id = SecureRandom.uuid
     @publish_date = publish_date
     @archived = archived
@@ -25,6 +29,3 @@ class Item
     @lablel = lablel
   end
 end
-
-new_item = Item.new(1)
-puts new_item.archived
