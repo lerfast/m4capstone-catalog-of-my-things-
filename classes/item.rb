892ba1f4a261ec24375d70b3dd62d07d8fb9762f
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-10-16 18:42:11 -0500

    Adding methods
    Add all methods visible in the diagram.
    Implement methods:
    can_be_archived?() in the Item class
    should return true if published_date is older than 10 years
    otherwise, it should return false
    move_to_archive() in the Item class
    should reuse can_be_archived?() method
    should change the archived property to true if the result of the can_be_archived?() method is true
    should do nothing if the result of the can_be_archived?() method is false

--- classes/item.rb
+++ classes/item.rb
@@ -1,8 +1,9 @@
 require 'securerandom'
+require 'date'
 
 class Item
-  attr_accessor :genre, :author, :source, :lablel
-  attr_reader :publish_date
+  attr_accessor :genre, :author, :source, :label
+  attr_reader :publish_date, :id, :archived
 
   def initialize(publish_date, archived: false)
     unless publish_date.match?(/^\d{4}-\d{2}-\d{2}$/)
@@ -10,7 +11,17 @@ class Item
     end
 
     @id = SecureRandom.uuid
-    @publish_date = publish_date
+    @publish_date = Date.parse(publish_date)
     @archived = archived
+  end
+
+  def can_be_archived?
+    (Date.today.year - @publish_date.year) > 10
+  end
+
+  def move_to_archive
+    if can_be_archived?
+      @archived = true
+    end
   end
 end
