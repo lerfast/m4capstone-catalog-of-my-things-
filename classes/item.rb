ff4e659b3f70179de6a7606beea356be1b14f509
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-10-18 12:40:13 -0400

    Update item.rb according reviewer suggetions

--- classes/item.rb
+++ classes/item.rb
@@ -2,12 +2,11 @@ require 'securerandom'
 require 'date'
 
 class Item
-  attr_accessor :source, :label
-  attr_reader :publish_date, :id, :archived
+  attr_accessor :source
+  attr_reader :publish_date, :id, :archived, :label, :genre, :author
 
   def initialize(publish_date, author = nil, archived: false)
     raise ArgumentError, 'Invalid publish_date format. Please use YYYY-MM-DD.' unless valid_date_format?(publish_date)
-
     @id = SecureRandom.uuid
     @publish_date = Date.parse(publish_date)
     @author = author
@@ -27,6 +26,11 @@ class Item
     author.add_item(self) unless author.items.include?(self)
   end
 
+  def label=(label)
+    @label = label
+    label.add_item(self) unless label.items.include?(self)
+  end
+
   def valid_date_format?(date)
     date.match?(/^\d{4}-\d{2}-\d{2}$/)
   end
@@ -37,7 +41,6 @@ class Item
 
   def move_to_archive
     return unless can_be_archived?
-
     @archived = true
   end
 end
