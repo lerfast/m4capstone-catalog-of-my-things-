44ca2d1453417f7df89e7d99be6f4cf1103abbc0
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-10-17 21:32:44 -0400

    Fix Linters

--- classes/label.rb
+++ classes/label.rb
@@ -1,19 +1,19 @@
 class Label
   attr_accessor :title, :color
   attr_reader :id, :items
-  
+
   def initialize(title:, color:, id: SecureRandom.random_number(1..1000), items: [])
     @id = id
     @title = title
     @color = color
     @items = items
   end
-  
+
   def add_item(item)
     @items.push(item)
     item.label = self
   end
-  
+
   def to_h
     {
       id: @id,
