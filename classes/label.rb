0b54e1f1c26d97f4bc8810de28a6c201ac3c8fc4
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-10-17 21:19:56 -0400

    Add the label class according the requirements

---
+++ classes/label.rb
@@ -0,0 +1,25 @@
+class Label
+  attr_accessor :title, :color
+  attr_reader :id, :items
+  
+  def initialize(title:, color:, id: SecureRandom.random_number(1..1000), items: [])
+    @id = id
+    @title = title
+    @color = color
+    @items = items
+  end
+  
+  def add_item(item)
+    @items.push(item)
+    item.label = self
+  end
+  
+  def to_h
+    {
+      id: @id,
+      title: @title,
+      color: @color,
+      items: @items.map(&:to_h)
+    }
+  end
+end
