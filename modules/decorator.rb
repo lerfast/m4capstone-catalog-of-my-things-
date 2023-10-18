e81eaadc79c491b9ce3d1228ff558a0d836ecda9
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-18 17:58:34 -0300

    update decorator to receive parameters

--- modules/decorator.rb
+++ modules/decorator.rb
@@ -4,12 +4,21 @@ require_relative '../classes/label'
 
 module Decorator
   def self.decorate(item)
-    item.author = Author.new('first', 'last')
-    item.genre = Genre.new('genre')
-    item.label = Label.new(title: 'label', color: 'color')
-    item
-    puts item.author.first_name
-    puts item.genre.name
-    puts item.label.title
+
+    puts 'first name of the author is:'
+    author_first_name = gets.chomp
+    puts 'last name of the author is:'
+    author_last_name = gets.chomp
+    item.author = Author.new(author_first_name, author_last_name)
+
+    puts 'name of the genre is:'
+    genre_name = gets.chomp
+    item.genre = Genre.new(genre_name)
+
+    puts 'title of the label is:'
+    label_title = gets.chomp
+    puts 'color of the label is:'
+    label_color = gets.chomp
+    item.label = Label.new(title: label_title, color: label_color)
   end
 end
