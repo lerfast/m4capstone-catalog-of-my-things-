c9249091367ae27f48bfa1fe2158ee6313f44f6b
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-18 19:06:50 -0300

    add a bit of style :D

--- modules/list.rb
+++ modules/list.rb
@@ -1,10 +1,12 @@
 module List
   def self.list_items(collection)
+    puts '----------------------------------------------'
     collection.each_with_index do |item, index|
       puts "#{index + 1}. Title: #{item.label.title} " \
            "Author: #{item.author.first_name} #{item.author.last_name} " \
            "Genre: #{item.genre.name} Publish date: #{item.publish_date}"
     end
+    puts '----------------------------------------------'
   end
 
   def self.list_genres(collection)
