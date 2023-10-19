3de451bef1773c05153e153cb1b8c92ab0b30547
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-19 15:35:21 -0300

    change item to avoid unnescessary parsing

--- classes/item.rb
+++ classes/item.rb
@@ -5,19 +5,12 @@ class Item
   attr_reader :publish_date, :id, :archived, :label, :genre, :author
 
   def initialize(publish_date, id: SecureRandom.uuid, archived: false, author: nil, genre: nil, label: nil)
-    unless valid_date_format?(publish_date)
-      raise ArgumentError,
-            'Invalid publish_date format. Please use YYYY-MM-DD.'
-    end
-
-    @publish_date = Date.parse(publish_date)
+    @publish_date = publish_date
     @id = id
     @archived = archived
     @author = author
     @genre = genre
     @label = label
-  rescue ArgumentError => e
-    puts e.message
   end
 
   def genre=(genre)
