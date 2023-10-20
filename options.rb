3ac5c46f1feefc0fd222729c9501d2e809e76489
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-20 14:06:03 -0300

    remove unnescessary options

--- options.rb
+++ options.rb
@@ -11,7 +11,6 @@ require_relative 'modules/save_label'
 require_relative 'modules/save_book'
 require_relative 'modules/save_game'
 require 'json'
-
 module Options
   include Decorator
   include List
@@ -22,20 +21,15 @@ module Options
       puts 'Please choose an option by entering a number:'
       puts '1 - List all books.'
       puts '2 - List all music albums.'
-      puts '3 - List all movies.'
-      puts '4 - List of games.'
-      puts '5 - List all genres.'
-      puts '6 - List all labels.'
-      puts '7 - List all authors.'
-      puts '8 - List all sources.'
-      puts '9 - Add a book.'
-      puts '10 - Add a music album.'
-      puts '11 - Add a movie.'
-      puts '12 - Add a game.'
+      puts '3 - List of games.'
+      puts '4 - List all genres.'
+      puts '5 - List all labels.'
+      puts '6 - List all authors.'
+      puts '7 - Add a book.'
+      puts '8 - Add a music album.'
+      puts '9 - Add a game.'
       puts '0 - Quit.'
-
       option = gets.chomp
-
       if number?(option)
         process_input(option.to_i)
       else
@@ -43,26 +37,22 @@ module Options
       end
     end
   end
-
   def number?(obj)
     obj = obj.to_s unless obj.is_a? String
     /\A[+-]?\d+(\.\d+)?\z/.match(obj)
   end
-
   OPTION_ACTIONS = {
     1 => :list_books,
     2 => :list_albums,
-    3 => :list_movies,
-    4 => :list_games,
-    5 => :list_genres,
-    6 => :list_labels,
-    7 => :list_authors,
-    9 => :add_book,
-    10 => :add_album,
-    12 => :add_game,
+    3 => :list_games,
+    4 => :list_genres,
+    5 => :list_labels,
+    6 => :list_authors,
+    7 => :add_book,
+    8 => :add_album,
+    9 => :add_game,
     0 => :quit
   }.freeze
-
   def process_input(option)
     action = OPTION_ACTIONS[option]
     if action
@@ -71,7 +61,6 @@ module Options
       show_error
     end
   end
-
   def list_books
     if @books.empty?
       puts 'No books available.'
@@ -83,7 +72,6 @@ module Options
       end
     end
   end
-
   def list_albums
     if @albums.empty?
       puts 'No albums available.'
@@ -91,7 +79,6 @@ module Options
       List.list_items(@albums)
     end
   end
-
   def list_games
     if @games.empty?
       puts 'No games available.'
@@ -99,11 +86,9 @@ module Options
       List.list_items(@games)
     end
   end
-
   def list_genres
     List.list_genres(@genres)
   end
-
   def list_authors
     if @authors.empty?
       puts 'No authors available.'
@@ -113,7 +98,6 @@ module Options
       end
     end
   end
-
   def list_labels
     if @labels.empty?
       puts 'No labels available.'
@@ -125,7 +109,6 @@ module Options
       end
     end
   end
-
   def add_album
     album_publish_date = verify_publish_date
     album = MusicAlbum.new(album_publish_date)
@@ -136,15 +119,12 @@ module Options
     puts 'Album added successfully!'
     puts '----------------------------------------------'
   end
-
   def add_book
     print 'Publisher: '
     publisher = gets.chomp
-
     while true
       print 'Cover state (select 1 for "good" or 2 for "bad" ): '
       cover_option = gets.chomp.to_i
-
       case cover_option
       when 1 then cover_state = 'good'
       when 2 then cover_state = 'bad'
@@ -154,75 +134,59 @@ module Options
       end
       break
     end
-
     publish_date = verify_publish_date('Enter the publish date of the book (YYYY-MM-DD):')
-
     book = Book.new(publisher: publisher, cover_state: cover_state, publish_date: publish_date)
     Decorator.decorate(book, @authors, @genres, @labels)
+    puts '----------------------------------------------'
     puts 'Book added successfully.'
+    puts '----------------------------------------------'
     @books << book
     SaveBook.save_book(book)
   end
-
   def add_game
     game_publish_date = verify_publish_date
-
     puts 'Thats the game has multiplayer? (y/n)'
     game_multiplayer = gets.chomp
-
     game_last_played_date = verify_publish_date('Enter the last played date of the game (YYYY-MM-DD):')
-
     game = Game.new(game_publish_date, game_multiplayer, game_last_played_date)
     Decorator.decorate(game, @authors, @genres, @labels)
     @games << game
     SaveGame.save_game_to_json(game)
-
     puts '----------------------------------------------'
     puts 'Game added successfully!!!'
     puts '----------------------------------------------'
   end
-
-  # Añade este método de validación al módulo Options
   def valid_date?(date_str)
     date_str.match?(/^\d{4}-\d{2}-\d{2}$/)
   end
-
   def verify_publish_date(prompt = 'Enter the publish date of the item (YYYY-MM-DD):')
     publish_date = ''
     loop do
       puts prompt
       publish_date = gets.chomp
       break if valid_date?(publish_date)
-
       puts 'Invalid date! Please enter a valid date in the format YYYY-MM-DD.'
     end
     publish_date
   end
-
   def load_games_from_json
     SaveGame.load_games_from_json
   end
-
   def load_albums_from_json
     SaveAlbum.load_albums
   end
-
   def load_genres_from_json
     SaveGenre.load_genres
   end
-
   def load_labels_from_json
     SaveLabel.load_labels
   end
-
   def load_books_from_json
     SaveBook.load_books
   end
-
   def show_error
     puts 'Error! Please select a valid option.'
   end
-
   def quit
     puts 'Saving your data ...'
     exit
