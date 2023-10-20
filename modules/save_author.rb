af7bc48976576eb26e161511af44b8c4ad2fe261
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-10-20 16:02:13 -0500

    Running Linters

--- modules/save_author.rb
+++ modules/save_author.rb
@@ -1,25 +1,24 @@
 module SaveAuthor
-    def self.save_author(author)
-      data = {
-        first_name: author.first_name,
-        last_name: author.last_name
-      }
-  
-      File.open('data/authors.json', 'a') do |file|
-        file.puts(data.to_json)
-      end
-    end
-  
-    def self.load_authors
-      authors = []
-      File.open('data/authors.json', 'r') do |file|
-        file.each_line do |line|
-          data = JSON.parse(line)
-          author = Author.new(data['first_name'], data['last_name'])
-          authors << author
-        end
-      end
-      authors
+  def self.save_author(author)
+    data = {
+      first_name: author.first_name,
+      last_name: author.last_name
+    }
+
+    File.open('data/authors.json', 'a') do |file|
+      file.puts(data.to_json)
     end
   end
-  
+
+  def self.load_authors
+    authors = []
+    File.open('data/authors.json', 'r') do |file|
+      file.each_line do |line|
+        data = JSON.parse(line)
+        author = Author.new(data['first_name'], data['last_name'])
+        authors << author
+      end
+    end
+    authors
+  end
+end
