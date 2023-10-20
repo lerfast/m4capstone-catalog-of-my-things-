d60d771bfbb681c36dc8abeac52cedb489d327ab
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-20 14:14:41 -0300

    added the rest of the sql

--- schema.sql
+++ schema.sql
@@ -8,6 +8,19 @@ CREATE TABLE public."Genre"
     PRIMARY KEY (id)
 );
 
+-- Label table
+CREATE TABLE label(
+  ID SERIAL PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+-- Author table
+CREATE TABLE author (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR
+);
+
 -- Item table
 CREATE TABLE public."Item"
 (
@@ -31,4 +44,37 @@ CREATE TABLE public."MusicAlbum"
     on_spotify boolean,
     PRIMARY KEY (id),
     FOREIGN KEY (item_id) REFERENCES public."Item"
+);
+
+-- Book table
+CREATE TABLE book(
+  ID SERIAL PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  publisher VARCHAR(30) NOT NULL,
+  cover_state VARCHAR(10) NOT NULL,
+  label_ID INT,
+  author_ID INT,
+  genre_ID INT,
+  FOREIGN KEY (label_ID) REFERENCES label(ID),
+  FOREIGN KEY (author_ID) REFERENCES author(ID),
+  FOREIGN KEY(genre_ID) REFERENCES genre(ID)
+);
+
+-- Game table
+CREATE TABLE games (
+    id TEXT PRIMARY KEY, -- UUID
+    name TEXT NOT NULL,
+    publish_date DATE NOT NULL,
+    genre_id INTEGER,
+    source_id INTEGER,
+    label_id INTEGER,
+    author_id INTEGER,
+    archived BOOLEAN DEFAULT FALSE,
+    multiplayer BOOLEAN, -- Solo es relevante para juegos, podría ser NULL para otros items
+    last_played_at DATE, -- Solo es relevante para juegos, podría ser NULL para otros items
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (source_id) REFERENCES sources (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id),
+    FOREIGN KEY (author_id) REFERENCES authors (id)
 );
