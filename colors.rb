4e216da7b4120e073178a323eb520948603294ca
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-21 13:06:45 -0300

    fix linters and erase unessesary file

--- colors.rb
+++
@@ -1,5 +0,0 @@
-require 'colorize'
-
-String.colors                       # return array of all possible colors names
-String.modes                        # return array of all possible modes
-String.color_samples   
