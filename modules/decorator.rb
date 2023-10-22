04ec579cc730d563b337b87059315f77255c9bcc
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-22 20:47:37 -0300

    fix linters

--- modules/decorator.rb
+++ modules/decorator.rb
@@ -4,38 +4,61 @@ require_relative '../classes/label'
 
 module Decorator
   def self.decorate(item, authors, genres, labels)
+    author_first_name, author_last_name = author_info
+    item.author = find_or_create_author(authors, author_first_name, author_last_name)
+
+    genre_name = genre_info
+    item.genre = find_or_create_genre(genres, genre_name)
+
+    label_title, label_color = label_info
+    item.label = find_or_create_label(labels, label_title, label_color)
+  end
+
+  def self.author_info
     puts 'first name of the author is:'
     author_first_name = gets.chomp
     puts 'last name of the author is:'
     author_last_name = gets.chomp
+    [author_first_name, author_last_name]
+  end
 
-    if authors.any? { |author| author.first_name == author_first_name && author.last_name == author_last_name }
-      item.author = authors.find { |author| author.first_name == author_first_name && author.last_name == author_last_name }
-    else
-      item.author = Author.new(author_first_name, author_last_name)
-      authors << item.author
-    end
+  def self.find_or_create_author(authors, first_name, last_name)
+    author = authors.find { |a| a.first_name == first_name && a.last_name == last_name }
+    return author if author
 
+    new_author = Author.new(first_name, last_name)
+    authors << new_author
+    new_author
+  end
+
+  def self.genre_info
     puts 'name of the genre is:'
-    genre_name = gets.chomp
+    gets.chomp
+  end
 
-    if genres.any? { |genre| genre.name == genre_name }
-      item.genre = genres.find { |genre| genre.name == genre_name }
-    else
-      item.genre = Genre.new(genre_name)
-      genres << item.genre
-    end
+  def self.find_or_create_genre(genres, genre_name)
+    genre = genres.find { |g| g.name == genre_name }
+    return genre if genre
 
+    new_genre = Genre.new(genre_name)
+    genres << new_genre
+    new_genre
+  end
+
+  def self.label_info
     puts 'title of the label is:'
     label_title = gets.chomp
     puts 'color of the label is:'
     label_color = gets.chomp
+    [label_title, label_color]
+  end
 
-    if labels.any? { |label| label.title == label_title && label.color == label_color }
-      item.label = labels.find { |label| label.title == label_title && label.color == label_color }
-    else
-      item.label = Label.new(title: label_title, color: label_color)
-      labels << item.label
-    end
+  def self.find_or_create_label(labels, title, color)
+    label = labels.find { |l| l.title == title && l.color == color }
+    return label if label
+
+    new_label = Label.new(title: title, color: color)
+    labels << new_label
+    new_label
   end
 end
--- modules/list_options.rb
+++ modules/list_options.rb
@@ -4,12 +4,12 @@ module ListOptions
 █▀▄ █▀▀ █░░ █▀▀ ▀█▀ █▀▀   ▄▀█ █▄░█   █ ▀█▀ █▀▀ █▀▄▀█ ▀█
 █▄▀ ██▄ █▄▄ ██▄ ░█░ ██▄   █▀█ █░▀█   █ ░█░ ██▄ █░▀░█ ░▄".light_blue
     loop do
-      puts "Select an item by index to delete or press 0 to go back."
+      puts 'Select an item by index to delete or press 0 to go back.'
       number = gets.chomp.to_i
-      if number == 0
+      if number.zero?
         return
       elsif number > collection.size
-        puts "Invalid index."
+        puts 'Invalid index.'
       else
         collection.delete_at(number - 1)
         puts "
@@ -23,5 +23,4 @@ module ListOptions
       end
     end
   end
-
 end
